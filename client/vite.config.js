cfe9d7f6835a522c5b87537c7da215e51e74937c
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-08-03 10:37:15 +0200

    fixing api url

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     port: 3000,
     proxy: {
-      '/api': 'http://localhost:5001',
+      '/api': 'https://pop-pursuit-api.vercel.app',
     },
   },
 });
