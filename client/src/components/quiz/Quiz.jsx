9acb9d5740c89f26ab4ac0153a740544ca7768b1
Roxanne Lucas <lucas.roxanne@gmail.com>
2023-08-07 18:29:45 +0200

    delete unused code

--- client/src/components/quiz/Quiz.jsx
+++ client/src/components/quiz/Quiz.jsx
@@ -1,12 +1,9 @@
-import React, { useContext } from 'react';
-import { QuizContext } from '../../contexts/QuizContext';
+import React from 'react';
 import Question from '../questions/Question';
 import History from '../history/History';
-import EndGame from '../endGame/EndGame';
 import './quiz.css';
 
 const Quiz = () => {
-  const { isGameEnded } = useContext(QuizContext);
   return (
     <div className='quiz-container'>
       <History />
